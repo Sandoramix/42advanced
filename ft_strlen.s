b07f9f64ffaa454cd80e50d16cfabe17c8893b54
odudniak <odudniak@student.42firenze.it>
2026-01-18 15:54:53 +0100

    added docs

--- ft_strlen.s
+++ ft_strlen.s
@@ -12,14 +12,18 @@ section .text
 	global ft_strlen
 
 ft_strlen:
+	; set rax to 0
 	xor	rax, rax
+	; check if str is null
 	cmp	rdi, 0
 		je	.end
 	.loop:
+		; load the byte at str[rax] into bl (8 bit register / 1 char)
 		mov	bl, [rdi + rax]
 		cmp	bl, 0
 			je	.end
 		inc	rax
 		jmp	.loop
 	.end:
+		; return rax
 		ret
--- libasm.h
+++ libasm.h
@@ -7,5 +7,10 @@
 # include <stdint.h>
 
 size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *dst, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+void	ft_write(int fd, const void *buf, size_t count);
+ssize_t	ft_read(int fd, void *buf, size_t count);
+char	*ft_strdup(const char *s1);
 
 #endif
